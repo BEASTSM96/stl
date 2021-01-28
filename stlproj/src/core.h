4583ef78d0e40289f8f5d5eb89675463f9d84a58
BEASTSM96 <beastboioioi.dev@gmail.com>
2021-01-28 18:18:30 +0000

    Added Updates for linux

--- stlproj/src/core.h
+++ stlproj/src/core.h
@@ -5,10 +5,16 @@
 
 #ifdef _STL_COMPILER_PREPROCESSOR_
 
-#ifndef _STL_INTERNAL_ASSERT_
-#define  _STL_INTERNAL_ASSERT_(cond, msg) if (!(cond)) { printf( "Assertion Failed: Message: %s, File %s, Line %i, Assertion on-hit time: %s ", msg, __FILE__, __LINE__, __TIME__ ); __debugbreak(); }
-#endif // !_STL_INTERNAL_ASSERT_
+#if defined( _WINDOWS_ ) || defined( _WIN64 )
+#define _STL_INTERNAL_BREAKDEBUG_() __debugbreak();
+#elif __linux__
+#include <signal.h>
+#define _STL_INTERNAL_BREAKDEBUG_() raise(SIGTRAP);
+#endif
 
+#ifndef _STL_INTERNAL_ASSERT_
+#define  _STL_INTERNAL_ASSERT_(cond, msg) if (!(cond)) { printf( "Assertion Failed: Message: %s, File %s, Line %i, Assertion on-hit time: %s ", msg, __FILE__, __LINE__, __TIME__ ); _STL_INTERNAL_BREAKDEBUG_(); }
+#endif // !_STL_INTERNAL_ASSERT_
 
 #define _STL_ASSERT_(cond, msg) _STL_INTERNAL_ASSERT_(cond, msg)
 
